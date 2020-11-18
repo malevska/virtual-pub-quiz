0e264941d71e89e6276a68a0f28c726eb65865f3
Dushko Jordanovski <jordanovskid@gmail.com>
2020-11-18 12:39:31 +0100

    Removed immer, reformatted

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
 
 const App = () => {
-    return <div>Hello World</div>
-}
+  return <div>Hello World</div>;
+};
 
-render(<App/>, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
