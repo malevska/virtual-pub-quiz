5f790d9fccb88afbc90fe27dbfd4c2152a9be268
Natasha Malevska <natasha@nutmeg.local>
2020-12-10 17:13:55 +0100

    view question layout

--- src/components/ViewQuestionComponent.tsx
+++ src/components/ViewQuestionComponent.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { useState } from "react";
-import { Button, Pane, TextInput, Heading, Radio } from "evergreen-ui";
+import {
+  Button,
+  Pane,
+  TextInput,
+  Heading,
+  Radio,
+  majorScale,
+} from "evergreen-ui";
 import { Question } from "../store/types";
 import ReactPlayer from "react-player";
-import { QuestionComponent } from "./QuestionComponent";
 
 //Shows the question and the list of players that may answer it
 export const ViewQuestionComponent = ({
@@ -20,39 +26,75 @@ export const ViewQuestionComponent = ({
   const [answerer, setAnswerer] = useState<number>(-1);
 
   return (
-    <Pane padding="20px">
-      <Heading size={900} marginTop="default">
-        {question.text}
-      </Heading>
-      {question.embedsType === "none" ? null : question.embedsType ===
-        "photo" ? (
-        <img src={question.embeds}></img>
-      ) : (
-        <ReactPlayer url={question.embeds} />
-      )}
-      <Button onClick={() => setShowAnswer(false)}>Show Answer</Button>
-      <Heading hidden={showAnswer} size={900} marginTop="default">
-        {question.answer}
-      </Heading>
-      <Pane role="players">
-        {players.map((pl, ind) => {
-          return (
-            <Radio
-              key={ind}
-              name="players"
-              label={pl}
-              onClick={() => setAnswerer(ind)}
-            />
-          );
-        })}
+    <Pane
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      flex="1 0 auto"
+      height="100%"
+      marginBottom="0"
+    >
+      <Pane
+        margin={majorScale(11)}
+        padding={majorScale(3)}
+        flexWrap="wrap"
+        flex="1 0 auto"
+        background="white"
+        width="70%"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        borderRadius={majorScale(1)}
+      >
+        <Heading
+          size={900}
+          marginTop={majorScale(8)}
+          marginBottom={majorScale(4)}
+          color="#363336"
+        >
+          {question.text}
+        </Heading>
+
+        {question.embedsType === "none" ? null : question.embedsType ===
+          "photo" ? (
+          <img src={question.embeds} />
+        ) : (
+          <ReactPlayer url={question.embeds} />
+        )}
+
+        <Button margin={majorScale(4)} onClick={() => setShowAnswer(false)}>
+          Show Answer
+        </Button>
+
+        <Heading hidden={showAnswer} size={900} margin={majorScale(4)}>
+          {question.answer}
+        </Heading>
       </Pane>
-      <TextInput
-        defaultValue={points}
-        onBlur={(e: any) => {
-          setPoints(parseInt(e.target.value, 10));
-        }}
-      />
-      <Button onClick={() => onClose(answerer, points)}>OK</Button>
+
+      <Pane>
+        <Pane role="players">
+          {players.map((pl, ind) => {
+            return (
+              <Radio
+                key={ind}
+                name="players"
+                label={pl}
+                onClick={() => setAnswerer(ind)}
+              />
+            );
+          })}
+
+          <TextInput
+            defaultValue={points}
+            onBlur={(e: any) => {
+              setPoints(parseInt(e.target.value, 10));
+            }}
+          />
+          <Button onClick={() => onClose(answerer, points)}>OK</Button>
+        </Pane>
+      </Pane>
     </Pane>
   );
 };
